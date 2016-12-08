03a0580364ca16cf0ca268e0f095a1babc17af0f
danko <danko@192.168.201.100>
2016-12-08 14:58:42 +0100

    Updated JSF to support new functions... Let's try shall we:D

---
+++ Library/src/web/AddNewBookBean.java
@@ -0,0 +1,53 @@
+package web;
+
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+import dal.BookFacade;
+import entities.Book;
+
+@ManagedBean(name="newBook", eager=true)
+public class AddNewBookBean implements Serializable{
+
+	private static final long serialVersionUID = 355580033880114009L;
+
+	private Book newBook;
+	private String Title;
+	private String ISBN;
+	
+	@EJB
+	private BookFacade bookFacade;
+
+	public void AddNewBook()
+	{
+		if(!Title.isEmpty() && !ISBN.isEmpty())
+		{
+			newBook = new Book();
+			newBook.setISBN(ISBN);
+			newBook.setTitle(Title);
+			
+			bookFacade.create(newBook);
+		}
+	}
+	
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	
+	
+	
+}
